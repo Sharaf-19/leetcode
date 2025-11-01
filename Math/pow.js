e374e6d38ca6afe28ad2931f646dd50337bc40cf
Sharaf Abdulwahab <136379283+Sharaf-19@users.noreply.github.com>
2025-11-01 14:50:47 +0000

    solved power(x,n)

---
+++ Math/pow.js
@@ -0,0 +1,48 @@
+// 50. Pow(x, n)
+
+// Medium
+
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+ 
+
+// Example 1:
+
+// Input: x = 2.00000, n = 10
+// Output: 1024.00000
+// Example 2:
+
+// Input: x = 2.10000, n = 3
+// Output: 9.26100
+// Example 3:
+
+// Input: x = 2.00000, n = -2
+// Output: 0.25000
+// Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+// SOLUTION:
+
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if (n === 0) return 1
+
+    if( n < 0){
+        n = -n
+        x = 1/x
+    }
+
+    let res = 1.0
+
+    while(n){
+        if(n%2 !== 0){
+            res *= x
+        }
+        x *= x
+        n = Math.floor(n/2)
+    }
+    return res
+};
